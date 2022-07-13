f9a1ef6d5c0091a1e94ebc2b0fdff183255714d9
Leoni Cesar <leonecezar99@gmail.com>
2022-07-12 21:27:49 -0300

    created initial project

---
+++ src/main/java/br/com/pedidos/dto/StatusDto.java
@@ -0,0 +1,15 @@
+package br.com.pedidos.dto;
+
+import br.com.pedidos.model.Status;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class StatusDto {
+    private Status status;
+}
---
+++ src/main/java/br/com/pedidos/model/Status.java
@@ -0,0 +1,12 @@
+package br.com.pedidos.model;
+
+public enum Status {
+    REALIZADO,
+    CANCELADO,
+    PAGO,
+    NAO_AUTORIZADO,
+    CONFIRMADO,
+    PRONTO,
+    SAIU_PARA_ENTREGA,
+    ENTREGUE;
+}
